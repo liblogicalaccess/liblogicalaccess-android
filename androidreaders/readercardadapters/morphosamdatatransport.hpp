79a93baf2e29ffa3c3986771a9635bf6ad59dc6f
Kapp Arnaud <kapp.arno@gmail.com>
2018-03-09 16:22:52 +0100

    morphoo work

---
+++ androidreaders/readercardadapters/morphosamdatatransport.hpp
@@ -0,0 +1,16 @@
+//
+// Created by xaqq on 3/9/18.
+//
+
+#ifndef RFIDGUARD_MORPHOSAMDATATRANSPORT_HPP
+#define RFIDGUARD_MORPHOSAMDATATRANSPORT_HPP
+
+
+
+class morphosamdatatransport {
+
+};
+
+
+
+#endif //RFIDGUARD_MORPHOSAMDATATRANSPORT_HPP
